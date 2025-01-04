6c7c58efaf5cef023d4f12de8467ef772e50b28c
NV-Phong <0h9ng03@gmail.com>
2025-01-05 00:48:58 +0700

    CONF - enum types

---
+++ src/types/emotion.enum.ts
@@ -0,0 +1,9 @@
+export enum Emotion {
+   SMIRK = '😏',
+   CLOWN = '🤡',
+   LAUGH = '😂',
+   SAD = '😢',
+   ANGRY = '😡',
+   LIKE = '👍',
+   LOVE = '❤️',
+}
---
+++ src/types/message-type.enum.ts
@@ -0,0 +1,7 @@
+export enum MessageType {
+   TEXT = 'TEXT',
+   FILE = 'FILE',
+   IMAGE = 'IMAGE',
+   VOID = 'VOID',
+   VIDEO = 'VIDEO',
+}
