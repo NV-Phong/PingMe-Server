1eb0a0378c251a80c80f91a3b9fe0932f25a47cd
NV-Phong <0h9ng03@gmail.com>
2025-01-05 14:09:46 +0700

    CHORE - format code

--- src/friendrequest/friendrequest.module.ts
+++ src/friendrequest/friendrequest.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { FriendRequest, FriendRequestSchema } from 'src/schema/friendrequest.schema';
+import {
+   FriendRequest,
+   FriendRequestSchema,
+} from 'src/schema/friendrequest.schema';
 import { FriendRequestController } from './friendrequest.controller';
 import { FriendRequestService } from './friendrequest.service';
 import { User, UserSchema } from 'src/schema/use.schema';
 
-
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: FriendRequest.name, schema: FriendRequestSchema },{ name: User.name, schema: UserSchema },]),
-  ],
-  controllers: [FriendRequestController],
-  providers: [FriendRequestService]
+   imports: [
+      MongooseModule.forFeature([
+         { name: FriendRequest.name, schema: FriendRequestSchema },
+         { name: User.name, schema: UserSchema },
+      ]),
+   ],
+   controllers: [FriendRequestController],
+   providers: [FriendRequestService],
 })
 export class FriendrequestModule {}
