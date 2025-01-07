ca06a7d3a1425b3a2e911967f6f41f76371ade1a
Phuocpvp <phuoc3003pham@gmail.com>
2025-01-07 22:44:53 +0700

    FEAT - create chat and add service load data from server

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Chat } from 'src/schema/chat.schema';
 import { Message } from 'src/schema/message.schema';
 import { MessageStatus } from 'src/types/message-status.enum';
@@ -18,7 +18,7 @@ export class ChatService {
       senderId: string,
       room: string,
       content: string,
-      IDReceiver:string,
+      IDReceiver: string,
    ): Promise<Message> {
       const newMessage = new this.messageModel({
          Message: content,
@@ -47,5 +47,58 @@ export class ChatService {
             IsDeleted: false, // Nếu bạn muốn loại bỏ các chat đã bị xóa
          })
          .exec();
+   }
+
+   async createOrFindPersonalChat(
+      userId: string,
+      targetId: string,
+   ): Promise<Chat> {
+      // Kiểm tra xem đoạn chat giữa hai người đã tồn tại chưa
+      let chat = await this.chatModel.findOne({
+         ChatType: 'PERSONAL',
+         GroupChat: {
+            $all: [
+               { $elemMatch: { UserId: userId } },
+               { $elemMatch: { UserId: targetId } },
+            ],
+         },
+      });
+
+      if (!chat) {
+         // Nếu chưa tồn tại, tạo mới đoạn chat
+         chat = new this.chatModel({
+            ChatName: `${userId}-${targetId}`, // Tên chat tạm thời
+            ChatType: 'PERSONAL',
+            GroupChat: [
+               { UserId: new Types.ObjectId(userId) },
+               { UserId: new Types.ObjectId(targetId) },
+            ],
+         });
+
+         await chat.save(); // Lưu chat vào MongoDB
+      }
+
+      return chat;
+   }
+
+   // Lấy chi tiết đoạn chat và lịch sử tin nhắn
+   async loadChat(
+      chatId: string,
+   ): Promise<{ chat: Chat; messages: Message[] }> {
+      console.log(chatId);
+      // Lấy thông tin đoạn chat
+      const chat = await this.chatModel.findById(chatId).exec();
+
+      if (!chat) {
+         throw new Error('Chat not found');
+      }
+
+      // Lấy danh sách tin nhắn của đoạn chat
+      const messages = await this.messageModel
+         .find({ IdChat: chatId })
+         .sort({ createdAt: 1 }) // Sắp xếp theo thời gian
+         .exec();
+
+      return { chat, messages };
    }
 }
